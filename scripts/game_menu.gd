889cfed440d1946e34d1a4f3a923f9109d21ee5d
drawiks <147206917+drawiks@users.noreply.github.com>
2025-09-13 03:49:37 +0300

    fix: buttons freeze

--- scripts/game_menu.gd
+++ scripts/game_menu.gd
@@ -1,9 +1,15 @@
 
 extends Control
 
+func _ready() -> void:
+	$panel/company_button.disabled = false
+
 func _on_company_button_pressed() -> void:
+	$panel/company_button.disabled = true
+	
 	$click_sound.play()
 	await $click_sound.finished
+	
 	get_tree().change_scene_to_file("res://scenes/company.tscn")
 
 func _on_back_button_pressed() -> void:
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,15 +3,24 @@ extends Control
 
 func _ready() -> void:
 	AudioManager.play_audio(preload("res://assets/sounds/menu.mp3"))
+	
+	$panel/play_button.disabled = false
+	$panel/shop_button.disabled = false
 
 func _on_play_button_pressed() -> void:
+	$panel/play_button.disabled = true
+	
 	$click_sound.play()
 	await $click_sound.finished
+	
 	get_tree().change_scene_to_file("res://scenes/game_menu.tscn")
 	
 func _on_shop_button_pressed() -> void:
+	$panel/shop_button.disabled = true
+	
 	$click_sound.play()
 	await $click_sound.finished
+	
 	get_tree().change_scene_to_file("res://scenes/shop_menu.tscn")
 	
 func _on_quit_button_pressed() -> void:
